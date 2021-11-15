23505e262b5088123633cc848f4730d3e22f457b
saki <16546851+sakiko-2@users.noreply.github.com>
2021-11-16 03:15:13 +1300

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
 import React, { useState } from 'react';
 import dompurify from 'dompurify';
 import marked from 'marked';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const Wrapper = styled.div`
+  display: grid;
+  flex: 1;
+  grid-template: 1fr / 1fr 1fr;
+  grid-gap: 1rem;
+  max-height: 100vh;
+  padding: 1rem;
+
+  @media (max-width: 576px) {
+    grid-template: 1fr 1fr / 1fr;
+  }
+`;
+
+const Preview = styled.div`
+  overflow: auto;
+`;
 
 function App() {
   const [input, setInput] = useState<string>('');
@@ -14,16 +39,16 @@ function App() {
   );
 
   return (
-    <div className="App">
-      <div>
+    <StyledApp className="App">
+      <Wrapper>
         <textarea
           value={input}
           onChange={handleChange}
           placeholder="Write markdown here..."
         />
-        <div dangerouslySetInnerHTML={createMarkup(input)} />
-      </div>
-    </div>
+        <Preview dangerouslySetInnerHTML={createMarkup(input)} />
+      </Wrapper>
+    </StyledApp>
   );
 }
 
